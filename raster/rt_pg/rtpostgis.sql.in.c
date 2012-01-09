markles/PostGIS

commit 239488f49d592bbd67c4d9678e6706999680819b

Make _ST_Intersects(raster, raster) STRICT as per comments in #1412.
git-svn-id: http://svn.osgeo.org/postgis/trunk@8714 b70326c6-7e19-0410-871a-916f4a2858ee

raster/rt_pg/rtpostgis.sql.in.c
@@ -2965,7 +2965,7 @@ CREATE OR REPLACE FUNCTION st_samealignment(
 CREATE OR REPLACE FUNCTION _st_intersects(rast1 raster, nband1 integer, rast2 raster, nband2 integer)
 	RETURNS boolean
 	AS 'MODULE_PATHNAME', 'RASTER_intersects'
-	LANGUAGE 'C' IMMUTABLE
+	LANGUAGE 'C' IMMUTABLE STRICT
 	COST 1000;
 
 CREATE OR REPLACE FUNCTION st_intersects(rast1 raster, nband1 integer, rast2 raster, nband2 integer)

commit 036adc3cce075735888bc577090855038520df22

Added conditional check to ST_Intersects(raster, int, raster, int) to deal with STRICT being set on _st_intersects(raster, int, raster, int).  Fixes regression caused in r8714.
git-svn-id: http://svn.osgeo.org/postgis/trunk@8728 b70326c6-7e19-0410-871a-916f4a2858ee

raster/rt_pg/rtpostgis.sql.in.c
@@ -2970,9 +2970,10 @@ CREATE OR REPLACE FUNCTION _st_intersects(rast1 raster, nband1 integer, rast2 ra
 
 CREATE OR REPLACE FUNCTION st_intersects(rast1 raster, nband1 integer, rast2 raster, nband2 integer)
 	RETURNS boolean
-	AS $$ SELECT $1 && $3 AND _st_intersects($1, $2, $3, $4) $$
+	AS $$ SELECT $1 && $3 AND CASE WHEN $2 IS NULL OR $4 IS NULL THEN TRUE ELSE _st_intersects($1, $2, $3, $4) END $$
 	LANGUAGE 'SQL' IMMUTABLE
 	COST 1000;
+
 
 CREATE OR REPLACE FUNCTION st_intersects(rast1 raster, rast2 raster)
 	RETURNS boolean